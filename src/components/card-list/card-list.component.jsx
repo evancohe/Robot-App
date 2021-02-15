1a0bff02a152ddd7b2d8570256cceaa525721250
evancohe <evsdevsubs@outlook.com>
2021-02-15 16:49:54 +0800

    responsive changes

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -4,9 +4,6 @@ import './card-list.style.css'
 
 export const CardList = (props) => {
 return <div className='card-list'>
-    { 
-    props.robots.map(robot => (
-    <Card key = {robot.id} robot = {robot} />
-    ))
-    }</div>
+    {props.robots.map(robot => (<Card key = {robot.id} robot = {robot} />))}
+    </div>
 }
